c16c1c9305981ef6bff62376fe2c11a230cf7b85
Wykett <bruno.chami@gmail.com>
2020-10-22 15:26:28 +0800

    Added promotion data loader

--- line-smt-apis/init_scratchorg.ps1
+++ line-smt-apis/init_scratchorg.ps1
@@ -1,3 +1,4 @@
 param([string]$alias = (Get-Date).tostring("yyyyMMddhhmmss"))
 sfdx force:org:create -f config/project-scratch-def.json --setalias $alias --setdefaultusername
 sfdx force:source:push
+sfdx force:org:open
--- line-smt-apis/init_scratchorg.sh
+++ line-smt-apis/init_scratchorg.sh
@@ -1,3 +1,4 @@
 alias=`date '+%Y%m%d%H%M%S'`
 sfdx force:org:create -f config/project-scratch-def.json --setalias $alias --setdefaultusername
 sfdx force:source:push
+sfdx force:org:open
